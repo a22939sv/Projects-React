005b43122138e25da211152dcfd92bd08f95c911
a22939sv <a22939@svalero.com>
2023-03-11 14:03:56 +0100

    Update glovo.sql

--- react-node-app/server/database/glovo.sql
+++ react-node-app/server/database/glovo.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 02-03-2023 a las 10:38:54
--- Versión del servidor: 10.4.24-MariaDB
--- Versión de PHP: 7.4.29
+-- Tiempo de generación: 11-03-2023 a las 14:02:49
+-- Versión del servidor: 10.4.27-MariaDB
+-- Versión de PHP: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -26,36 +26,139 @@ USE `glovo`;
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `menu`
+--
+
+CREATE TABLE `menu` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `menu`
+--
+
+INSERT INTO `menu` (`id`, `name`) VALUES
+(1, 'Menu Americana'),
+(2, 'Menu Supermercado');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `producto`
+--
+
+CREATE TABLE `producto` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `price` decimal(5,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `producto`
+--
+
+INSERT INTO `producto` (`id`, `name`, `description`, `price`) VALUES
+(1, 'Happy Meal', 'Menu Infantil (Hamburguesa o Nuggets)', '4.20'),
+(2, 'Nuggets 25 Uds', 'Cubo de nuggets con 25 unidades', '11.85'),
+(3, 'Patatas Fritas', 'Caja de patatas fritas grandes', '3.85');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `producto_menu`
+--
+
+CREATE TABLE `producto_menu` (
+  `idMenu` int(11) NOT NULL,
+  `idProducto` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `producto_menu`
+--
+
+INSERT INTO `producto_menu` (`idMenu`, `idProducto`) VALUES
+(1, 1),
+(1, 2),
+(1, 3);
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `restaurant`
 --
 
 CREATE TABLE `restaurant` (
   `id` int(11) NOT NULL,
-  `name` varchar(75) COLLATE utf8_spanish2_ci DEFAULT NULL,
-  `category` varchar(75) COLLATE utf8_spanish2_ci DEFAULT NULL,
+  `name` varchar(75) DEFAULT NULL,
+  `category` varchar(75) DEFAULT NULL,
   `star` int(1) DEFAULT NULL,
-  `img` text COLLATE utf8_spanish2_ci DEFAULT NULL
+  `img` text DEFAULT NULL,
+  `idMenu` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
 --
 -- Volcado de datos para la tabla `restaurant`
 --
 
-INSERT INTO `restaurant` (`id`, `name`, `category`, `star`, `img`) VALUES
-(1, 'McDonald\'s', 'Americana', 3, 'https://res.cloudinary.com/glovoapp/w_450,h_250,c_fill,f_auto,q_30/Stores/x5curfadsolebipl5iyh'),
-(2, 'Dia', 'Supermercado', 5, 'https://res.cloudinary.com/glovoapp/w_450,h_250,c_fill,f_auto,q_30/Stores/ly8eyexldlgqntcb0uyt'),
-(3, 'Burger King', 'Americana', 3, 'https://res.cloudinary.com/glovoapp/w_450,h_250,c_fill,f_auto,q_30/Stores/ie0obr3ckjemazfybypz'),
-(4, 'Telepizza', 'Pizza', 4, 'https://res.cloudinary.com/glovoapp/w_450,h_250,c_fill,f_auto,q_30/Stores/redheyfvhbgvedsdbmkb'),
-(5, 'Starbucks', 'Desayuno', 4, 'https://res.cloudinary.com/glovoapp/w_450,h_250,c_fill,f_auto,q_30/Stores/rol78tegsyjdf3yjcjma'),
-(6, 'Dunkin', 'Desayuno', 4, 'https://res.cloudinary.com/glovoapp/w_450,h_250,c_fill,f_auto,q_30/Stores/wap0jg2pkxnl5v0nj75b'),
-(7, 'King Doner Kebab', 'Árabe', 3, 'https://res.cloudinary.com/glovoapp/w_450,h_250,c_fill,f_auto,q_30/Stores/cqpw59aux0wmftouz1dc'),
-(8, 'Taco Bell', 'Mexicana', 4, 'https://res.cloudinary.com/glovoapp/w_450,h_250,c_fill,f_auto,q_30/Stores/rxbvf1ntbypud38z0dmy'),
-(9, 'Papa Johns', 'Pizza', 4, 'https://res.cloudinary.com/glovoapp/w_450,h_250,c_fill,f_auto,q_30/Stores/gvzgrpc77bgmmqd1lbcf'),
-(10, 'Goiko', 'Americana', 3, 'https://res.cloudinary.com/glovoapp/w_450,h_250,c_fill,f_auto,q_30/Stores/gdifyfyb3yhp8akmf5pv');
+INSERT INTO `restaurant` (`id`, `name`, `category`, `star`, `img`, `idMenu`) VALUES
+(1, 'McDonald\'s', 'Americana', 3, 'https://res.cloudinary.com/glovoapp/w_450,h_250,c_fill,f_auto,q_30/Stores/x5curfadsolebipl5iyh', 1),
+(2, 'Dia', 'Supermercado', 5, 'https://res.cloudinary.com/glovoapp/w_450,h_250,c_fill,f_auto,q_30/Stores/ly8eyexldlgqntcb0uyt', 2),
+(3, 'Burger King', 'Americana', 3, 'https://res.cloudinary.com/glovoapp/w_450,h_250,c_fill,f_auto,q_30/Stores/ie0obr3ckjemazfybypz', 1),
+(4, 'Telepizza', 'Pizza', 4, 'https://res.cloudinary.com/glovoapp/w_450,h_250,c_fill,f_auto,q_30/Stores/redheyfvhbgvedsdbmkb', 3),
+(5, 'Starbucks', 'Desayuno', 4, 'https://res.cloudinary.com/glovoapp/w_450,h_250,c_fill,f_auto,q_30/Stores/rol78tegsyjdf3yjcjma', 4),
+(6, 'Dunkin', 'Desayuno', 4, 'https://res.cloudinary.com/glovoapp/w_450,h_250,c_fill,f_auto,q_30/Stores/wap0jg2pkxnl5v0nj75b', 4),
+(7, 'King Doner Kebab', 'Árabe', 3, 'https://res.cloudinary.com/glovoapp/w_450,h_250,c_fill,f_auto,q_30/Stores/cqpw59aux0wmftouz1dc', 5),
+(8, 'Taco Bell', 'Mexicana', 4, 'https://res.cloudinary.com/glovoapp/w_450,h_250,c_fill,f_auto,q_30/Stores/rxbvf1ntbypud38z0dmy', 6),
+(9, 'Papa Johns', 'Pizza', 4, 'https://res.cloudinary.com/glovoapp/w_450,h_250,c_fill,f_auto,q_30/Stores/gvzgrpc77bgmmqd1lbcf', 3),
+(10, 'Goiko', 'Americana', 3, 'https://res.cloudinary.com/glovoapp/w_450,h_250,c_fill,f_auto,q_30/Stores/gdifyfyb3yhp8akmf5pv', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `id` int(11) NOT NULL,
+  `firstName` varchar(50) DEFAULT NULL,
+  `lastName` varchar(75) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `address` varchar(100) DEFAULT NULL,
+  `password` varchar(32) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `usuario`
+--
+
+INSERT INTO `usuario` (`id`, `firstName`, `lastName`, `email`, `address`, `password`) VALUES
+(1, 'Carlos', 'Crespo Cambra', 'a22939@svalero.com', 'C/Violeta Parra, 9', '123');
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `menu`
+--
+ALTER TABLE `menu`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `producto_menu`
+--
+ALTER TABLE `producto_menu`
+  ADD PRIMARY KEY (`idMenu`,`idProducto`),
+  ADD KEY `FK_PRODUCTO_MENU_PRODUCTO` (`idProducto`);
 
 --
 -- Indices de la tabla `restaurant`
@@ -64,14 +167,49 @@ ALTER TABLE `restaurant`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
+
+--
+-- AUTO_INCREMENT de la tabla `menu`
+--
+ALTER TABLE `menu`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `producto`
+--
+ALTER TABLE `producto`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `restaurant`
 --
 ALTER TABLE `restaurant`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `producto_menu`
+--
+ALTER TABLE `producto_menu`
+  ADD CONSTRAINT `FK_PRODUCTO_MENU_MENU` FOREIGN KEY (`idMenu`) REFERENCES `menu` (`id`),
+  ADD CONSTRAINT `FK_PRODUCTO_MENU_PRODUCTO` FOREIGN KEY (`idProducto`) REFERENCES `producto` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
