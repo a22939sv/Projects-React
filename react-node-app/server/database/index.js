dc57809e8d484f638df88f5912528fd14e65a910
a22939sv <a22939@svalero.com>
2023-03-17 09:28:55 +0100

    update

--- react-node-app/server/database/index.js
+++ react-node-app/server/database/index.js
@@ -1,10 +1,13 @@
-const mysql = require('mysql2')
+const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DBNAME
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DBNAME,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
-module.exports = pool.promise()
+module.exports = pool.promise();
